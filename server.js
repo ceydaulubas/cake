94d866df891ab8191a63f4e7881c650188dbecf3
Ceyda Ulubas <ceyda.ulubas@adcolony.com>
2022-09-07 13:16:19 +0300

    fixed the middleware

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = 9001;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
